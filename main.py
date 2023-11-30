39e7a55386f6278b997f6d1a61d374f35ae69c37
rayjay7225 <79537106+rayjay7225@users.noreply.github.com>
2023-11-30 10:38:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ pyautogui.FAILSAFE = False
 
 scr = pyautogui.size()
 
+print("LOCKED")
 i = 0
 while True:
     try:
+        pyautogui.hotkey("win", "pgdn")
         pyautogui.moveTo(scr[0]/2, scr[1]/2)
         sleep(1/50)
         if keyboard.is_pressed('ctrl') and keyboard.is_pressed('shift') and keyboard.is_pressed('h'):
+            print("UNLOCKED")
             exit()
     except KeyboardInterrupt:
         continue
